2dee8ae66fa7b60222f2cf9856ca6167be04819e
Negi310 <f.satopec@icloud.com>
2025-03-10 03:44:18 +0900

    ボーダー

--- Assets/Scripts/Player/Information_Player.cs
+++ Assets/Scripts/Player/Information_Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Information_Player : MonoBehaviour
 {
-    protected float playerSpeed = 0.08f;
+    protected float playerSpeed = 0.07f;
     protected float playerDashSpeed = 0.08f;
     protected float playerRollSpeed = 0.12f;
     protected float jumpForce = 18f;
